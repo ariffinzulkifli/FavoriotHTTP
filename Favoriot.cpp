ddf53d362333d8331004f942b4e451e061e74d44
Mohamad Ariffin Zulkifli <ariffinastute@live.com>
2020-02-12 22:40:19 +0800

    Update Favoriot.cpp

--- Favoriot.cpp
+++ Favoriot.cpp
@@ -1,7 +1,7 @@
 /*
-  Morse.cpp - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
+  Favoriot.cpp - Library for Favoriot Data Stream.
+  Created by Mohamad Ariffin Zulkifli, February 2, 2020.
+  MIT License.
 */
 
 #include "Arduino.h"
